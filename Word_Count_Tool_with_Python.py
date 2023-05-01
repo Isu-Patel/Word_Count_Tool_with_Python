25e80041002a0b79f1eb38c80b29d30f9df616ae
ISU <110623091+Isu-Patel@users.noreply.github.com>
2023-05-01 22:10:55 +0530

    Update and rename .gitignore to Word_Count_Tool_with_Python.py

---
+++ Word_Count_Tool_with_Python.py
@@ -0,0 +1,13 @@
+# Word Count Tool with Python
+
+# Prompt the user to enter a string
+string = input("Enter a string: ")
+
+# Split the string into words
+words = string.split()
+
+# Count the number of words
+word_count = len(words)
+
+# Display the word count to the user
+print(f"The word count of the string is: {word_count}")
